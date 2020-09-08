8ff33aa3cd0569e355c11e9dd666dafb00dffe96
skampala1 <skampala1@gmail.com>
2020-09-08 17:47:36 +0400

    ex1 and ex2

---
+++ ex1.js
@@ -0,0 +1,21 @@
+var temp = (((Math.random() * 8)) + 18);
+var opclose_boolean = Math.random() <=0.5 ;
+
+if (temp > 25)
+{
+    console.log("It is too hot... AC switched on")
+}
+else if (temp < 25)
+{
+    console.log("It is too cold.. AC switched off")
+}
+else if (opclose_boolean)
+{
+    console.log("Someone walked in...")
+    console.log("Welcome.. Lights switched on")
+}
+else if (!opclose_boolean)
+{
+    console.log("Someone left...")
+    console.log("Bye bye! Lights switched off")
+}
